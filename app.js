62ef9d10230a5ceb673acc50f489597848a6c579
Hoxton <za546321@yahoo.com.tw>
2023-10-03 01:28:56 +0800

    屬性綁定

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Hello vue')
+
+const app =Vue.createApp({
+    data(){
+        return{
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: '45'
+
+        }
+    }
+})
+app.mount('#app')
